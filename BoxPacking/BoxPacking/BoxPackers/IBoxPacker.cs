51d59cc2dad94301224ce1c116632e514c0673de
Ejnar Sörensen <ejnar.sorensen@gmail.com>
2021-03-17 22:20:53 +0100

    Add files via upload

---
+++ BoxPacking/BoxPacking/BoxPackers/IBoxPacker.cs
@@ -0,0 +1,12 @@
+﻿using BoxPacking.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BoxPacking
+{
+    public interface IBoxPacker
+    {
+        List<Box> Pack(List<Article> articles, int numBoxes);
+    }
+}
